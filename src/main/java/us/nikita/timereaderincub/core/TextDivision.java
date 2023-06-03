c3bfa5110381c4ec7b5a8559e8663e1a65c6ac3f
Nikita <ltkmabyf1979@GMAIL.COM>
2023-06-02 20:33:03 -0500

    fix TextDivision

--- src/main/java/us/nikita/timereaderincub/core/TextDivision.java
+++ src/main/java/us/nikita/timereaderincub/core/TextDivision.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TextDivision {
+    private static final List<String> EMPLOYEE_PREFIX = List.of("Operator", "Assistant");
    static ReadImage readImage = new ReadImage();
    static String textForDivision = readImage.readImage();
 
@@ -15,25 +16,32 @@ public class TextDivision {
 
 
     public static void main(String[] args){
-        String[] splitText = textForDivision.split("\n");
+        String[] splitText = textForDivision.split("\\n+");
 
-        for (int i = 0; i < splitText.length; i++) {
-            String line = splitText[i].trim();
-            String[] words = line.split("\\s");
-            if (words[0].equals("Operator") || words[0].equals("Assistent")) {
-                teamWorkers.add(Arrays.toString(words));
-                teamWorkers.remove(0);
-            } else  if(words[1].equals("Start")){
+        for (String s : splitText) {
+            String line = s.trim();
+            String[] words = line.split("\\s+");
+
+
+            if (hasEmployee(line)) {
+                teamWorkers.add(line);
+            } else
+                if (words[1].equals("Start")) {
                 cable.add(words[1]);
-            } else if (words[words.length-1].equals("ft")){
+            } else
+                if (words[words.length - 1].equals("ft")) {
                 try {
                     cableLength.add(Integer.valueOf(words[1]));
-                }catch (Exception e){
-                    System.out.println("Ошибка ввода числа" + e.getMessage());
+                } catch (Exception e) {
+                    System.out.println("not number" + e.getMessage());
                 }
             }
         }
        totalLegth = cableLength.stream().reduce(0,Integer::sum);
-        System.out.println(totalLegth);
+    }
+
+    private static boolean hasEmployee(String line){
+
+       return line.startsWith("Operator")|| line.startsWith("Assistant");
     }
 }
