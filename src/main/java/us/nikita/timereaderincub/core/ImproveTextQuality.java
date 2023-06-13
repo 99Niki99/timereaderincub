809a84b0c25866222ec6c20cc1a86ba7d75ca115
Nikita <ltkmabyf1979@GMAIL.COM>
2023-06-12 21:16:25 -0500

    refactoring

--- src/main/java/us/nikita/timereaderincub/core/ImproveTextQuality.java
+++ src/main/java/us/nikita/timereaderincub/core/ImproveTextQuality.java
@@ -8,7 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * that class takes an image, changes its color correction and saves it in another folder
+ * This class takes an image, resizes and colors it, and saves the new image in a separate folder.
  *
  * @author Nikita
  */
@@ -18,6 +18,14 @@ public class ImproveTextQuality {
     private static final int WIDTH = 1050;
     private static final int HEIGHT = 1024;
 
+    /**
+     * This method crops and saves the image in the specified folder.
+     *
+     * @param bufferImages empty image buffer
+     * @param scaleFactor  defined as the ratio between the scale of a given original object and a new object
+     * @param offset       It determines how high the brightness (magnification brightness) of the image is after scaling.
+     * @throws IOException usually occurs when there is a problem with reading or writing data.
+     */
     public static void
     processImg(BufferedImage bufferImages, float scaleFactor, float offset) throws IOException {
         BufferedImage makeNewImage = new BufferedImage(WIDTH, HEIGHT, bufferImages.getType());
@@ -31,31 +39,47 @@ public class ImproveTextQuality {
         RescaleOp makeGrayScaling = new RescaleOp(scaleFactor, offset, null);
 
         BufferedImage performingScaling = makeGrayScaling.filter(makeNewImage, null);
-        ImageIO.write(performingScaling, "jpg", new File("D:\\NewImage\\output2.png"));
+        ImageIO.write(performingScaling, "jpg", new File("D:\\NewImage\\output3.png"));
     }
 
-    public static void main(String[] args) throws Exception {
+    /**
+     * The method takes images and determines the axis of its change
+     *
+     * @throws IOException The exception may be caused by incorrect reading and writing of the image
+     */
+    public static void axisOfChangeSpecifier() throws IOException {
         File addressImageForChange = new File(
                 "D:\\PhotoForTest\\photoTest1.jpg");
 
         BufferedImage takeImageForChange = ImageIO.read(addressImageForChange);
 
-        double readSizeImage
+        double getSizeImage
                 = takeImageForChange
                 .getRGB(takeImageForChange.getTileWidth() / 2,
                         takeImageForChange.getTileHeight() / 2);
 
-        if (readSizeImage >= ScalingFactors.TYPE_ONE.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_ONE.getTopBorder()) {
+        if (getSizeImage >= ScalingFactors.TYPE_ONE.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_ONE.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_ONE.getScalingFactor(), ScalingFactors.TYPE_ONE.getOffset());
-        } else if (readSizeImage >= ScalingFactors.TYPE_TWO.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_TWO.getTopBorder()) {
+
+        } else if (getSizeImage >= ScalingFactors.TYPE_TWO.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_TWO.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_TWO.getScalingFactor(), ScalingFactors.TYPE_TWO.getOffset());
-        } else if (readSizeImage >= ScalingFactors.TYPE_THREE.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_THREE.getTopBorder()) {
+
+        } else if (getSizeImage >= ScalingFactors.TYPE_THREE.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_THREE.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_THREE.getScalingFactor(), ScalingFactors.TYPE_THREE.getOffset());
-        } else if (readSizeImage >= ScalingFactors.TYPE_FORE.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_FORE.getTopBorder()) {
+
+        } else if (getSizeImage >= ScalingFactors.TYPE_FORE.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_FORE.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_FORE.getScalingFactor(), ScalingFactors.TYPE_FORE.getOffset());
-        } else if (readSizeImage >= ScalingFactors.TYPE_FIVE.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_FIVE.getTopBorder()) {
+
+        } else if (getSizeImage >= ScalingFactors.TYPE_FIVE.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_FIVE.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_FIVE.getScalingFactor(), ScalingFactors.TYPE_FIVE.getOffset());
-        } else if (readSizeImage >= ScalingFactors.TYPE_SIX.getBottomBorder() && readSizeImage < ScalingFactors.TYPE_SIX.getTopBorder()) {
+
+        } else if (getSizeImage >= ScalingFactors.TYPE_SIX.getBottomBorder() && getSizeImage <
+                ScalingFactors.TYPE_SIX.getTopBorder()) {
             processImg(takeImageForChange, ScalingFactors.TYPE_SIX.getScalingFactor(), ScalingFactors.TYPE_SIX.getOffset());
         }
     }
