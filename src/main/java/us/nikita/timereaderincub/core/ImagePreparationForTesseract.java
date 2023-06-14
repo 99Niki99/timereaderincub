ac203d16681f9fc0603dc163666e11933a2ffd38
Nikita <ltkmabyf1979@GMAIL.COM>
2023-06-13 21:41:02 -0500

    refactoring

--- src/main/java/us/nikita/timereaderincub/core/ImagePreparationForTesseract.java
+++ src/main/java/us/nikita/timereaderincub/core/ImagePreparationForTesseract.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * This class takes an image, resizes and colors it, and saves the new image in a separate folder.
@@ -17,21 +18,41 @@ public class ImagePreparationForTesseract {
 
     private static final int WIDTH = 1050;
     private static final int HEIGHT = 1024;
-    private static final String PATH_FROM = "D:\\PhotoForTest\\photoTest1.jpg";
-    private static final String PATH_TO = "D:\\NewImage\\output3.png";
+    private static final Path PATH_FROM = Path.of("D:\\PhotoForTest\\photoTest1.jpg");
+    private static final Path PATH_TO = Path.of("D:\\NewImage\\output4.png");
+
+    /**
+     * The method takes images and determines the axis of its change
+     *
+     * @param from Returns the address where we get the image from
+     * @param to   Returns the address where we put the image
+     * @throws IOException The exception may be caused by incorrect reading and writing of the image
+     */
+    public static void processImage(Path from, Path to) throws IOException {
+
+        File addressImageForChange = from.toFile();
+
+        BufferedImage imageForChange = ImageIO.read(addressImageForChange);
+
+        ScalingFactors scalingFactor = ScalingFactors.getForImage(imageForChange);
+        cropAndSave(imageForChange, scalingFactor.getScalingFactor(), scalingFactor.getOffset(), to);
+
+    }
+
     /**
      * This method crops and saves the image in the specified folder.
      *
-     * @param bufferImages empty image buffer
-     * @param scaleFactor  defined as the ratio between the scale of a given original object and a new object
+     * @param bufferImages Empty image buffer
+     * @param scaleFactor  Defined as the ratio between the scale of a given original object and a new object
      * @param offset       It determines how high the brightness (magnification brightness) of the image is after scaling.
-     * @throws IOException usually occurs when there is a problem with reading or writing image.
+     * @param to           Returns the address where we put the image
+     * @throws IOException Usually occurs when there is a problem with reading or writing image.
      */
-    public static void
-    processImg(BufferedImage bufferImages, float scaleFactor, float offset) throws IOException {
-        BufferedImage makeNewImage = new BufferedImage(WIDTH, HEIGHT, bufferImages.getType());
+    private static void
+    cropAndSave(BufferedImage bufferImages, float scaleFactor, float offset, Path to) throws IOException {
+        BufferedImage newImage = new BufferedImage(WIDTH, HEIGHT, bufferImages.getType());
 
-        Graphics2D graphicForNewImage = makeNewImage.createGraphics();
+        Graphics2D graphicForNewImage = newImage.createGraphics();
 
         graphicForNewImage.drawImage(bufferImages, 0, 0,
                 WIDTH, HEIGHT, null);
@@ -39,49 +60,9 @@ public class ImagePreparationForTesseract {
 
         RescaleOp makeGrayScaling = new RescaleOp(scaleFactor, offset, null);
 
-        BufferedImage performingScaling = makeGrayScaling.filter(makeNewImage, null);
-        ImageIO.write(performingScaling, "jpg", new File(PATH_TO));
+        BufferedImage performingScaling = makeGrayScaling.filter(newImage, null);
+        ImageIO.write(performingScaling, "jpg", to.toFile());
     }
 
-    /**
-     * The method takes images and determines the axis of its change
-     *
-     * @throws IOException The exception may be caused by incorrect reading and writing of the image
-     */
-    public static void axisOfChangeSpecifier() throws IOException {
-        File addressImageForChange = new File(
-                PATH_FROM);
 
-        BufferedImage takeImageForChange = ImageIO.read(addressImageForChange);
-
-        double getSizeImage
-                = takeImageForChange
-                .getRGB(takeImageForChange.getTileWidth() / 2,
-                        takeImageForChange.getTileHeight() / 2);
-
-        if (getSizeImage >= ScalingFactors.TYPE_ONE.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_ONE.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_ONE.getScalingFactor(), ScalingFactors.TYPE_ONE.getOffset());
-
-        } else if (getSizeImage >= ScalingFactors.TYPE_TWO.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_TWO.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_TWO.getScalingFactor(), ScalingFactors.TYPE_TWO.getOffset());
-
-        } else if (getSizeImage >= ScalingFactors.TYPE_THREE.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_THREE.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_THREE.getScalingFactor(), ScalingFactors.TYPE_THREE.getOffset());
-
-        } else if (getSizeImage >= ScalingFactors.TYPE_FORE.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_FORE.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_FORE.getScalingFactor(), ScalingFactors.TYPE_FORE.getOffset());
-
-        } else if (getSizeImage >= ScalingFactors.TYPE_FIVE.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_FIVE.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_FIVE.getScalingFactor(), ScalingFactors.TYPE_FIVE.getOffset());
-
-        } else if (getSizeImage >= ScalingFactors.TYPE_SIX.getBottomBorder() && getSizeImage <
-                ScalingFactors.TYPE_SIX.getTopBorder()) {
-            processImg(takeImageForChange, ScalingFactors.TYPE_SIX.getScalingFactor(), ScalingFactors.TYPE_SIX.getOffset());
-        }
-    }
 }
--- src/main/java/us/nikita/timereaderincub/core/ScalingFactors.java
+++ src/main/java/us/nikita/timereaderincub/core/ScalingFactors.java
@@ -1,18 +1,21 @@
 package us.nikita.timereaderincub.core;
 
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
+
 public enum ScalingFactors {
 
-    TYPE_ONE (-1.4211511E7, -7254228, 3f, -10f),
-    TYPE_TWO (-7254228, -2171170, 1.455f, -47f),
+    TYPE_ONE(-1.4211511E7, -7254228, 3f, -10f),
+    TYPE_TWO(-7254228, -2171170, 1.455f, -47f),
     TYPE_THREE(-2171170, -1907998, 1.35f, 10f),
     TYPE_FORE(-1907998, -257, 1.19f, 0.5f),
     TYPE_FIVE(-257, -1, 1f, 0.5f),
-    TYPE_SIX(-1, 2, 1f,  0.35f);
+    TYPE_SIX(-1, 2, 1f, 0.35f);
 
-    private double bottomBorder;
-    private int topBorder;
-    private float scalingFactor;
-    private float offset;
+    private final double bottomBorder;
+    private final int topBorder;
+    private final float scalingFactor;
+    private final float offset;
 
     ScalingFactors(double bottomBorder, int topBorder, float scalingFactor, float offset) {
         this.bottomBorder = bottomBorder;
@@ -21,12 +24,18 @@ public enum ScalingFactors {
         this.offset = offset;
     }
 
-    public double getBottomBorder() {
-        return bottomBorder;
-    }
 
-    public int getTopBorder() {
-        return topBorder;
+    public static ScalingFactors getForImage(BufferedImage image) {
+        double imageSize
+                = image
+                .getRGB(image.getTileWidth() >> 1,
+                        image.getTileHeight() >> 1);
+
+        return Arrays.stream(ScalingFactors.values())
+                .filter(value -> imageSize >= value.bottomBorder && imageSize <
+                        value.topBorder)
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
     }
 
     public float getScalingFactor() {
