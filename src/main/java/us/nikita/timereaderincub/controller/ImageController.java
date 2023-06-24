6b28065caaad387a4d476a23ae5ca03285c3919d
99Niki99 <96220339+99Niki99@users.noreply.github.com>
2023-06-24 17:59:36 -0500

    Update ImageController.java
    refactor

--- src/main/java/us/nikita/timereaderincub/controller/ImageController.java
+++ src/main/java/us/nikita/timereaderincub/controller/ImageController.java
@@ -1,6 +1,6 @@
 package us.nikita.timereaderincub.controller;
 
-import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import us.nikita.timereaderincub.core.PathFromAndTo;
+import us.nikita.timereaderincub.core.ImagePreparationService;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 /**
  * controller download and upload image
@@ -23,7 +18,11 @@ import java.util.UUID;
  * @author Nikita
  */
 @Controller
-public class ImageController {
+public final class ImageController {
+
+    @Autowired
+    private ImagePreparationService service;
+
     /**
      * Upload Image
      *
@@ -31,22 +30,14 @@ public class ImageController {
      * @return image
      */
     @PostMapping("/convert")
-    public ResponseEntity<ByteArrayResource> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
-           String fileName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(String.valueOf(file));
-           String savePath = PathFromAndTo.getPathTo() + fileName;
-           File saveFile = new File(savePath);
-           file.transferTo(saveFile);
+    public final ResponseEntity<ByteArrayResource> convert (@RequestParam("file") MultipartFile file) throws IOException {
+        ByteArrayResource resource = service.prepare(file);
 
-           File converted = null;
-
-           BufferedImage image = ImageIO.read(converted);
-           ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-           ImageIO.write(image, "jpeg", byteArrayOutputStream);
-           ByteArrayResource imageResource = new ByteArrayResource(byteArrayOutputStream.toByteArray());
-
-           return ResponseEntity.ok()
-                   .contentType(MediaType.IMAGE_JPEG)
-                   .body(imageResource);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(resource);
     }
+
+}
 
 }
